e53062cbeb49b5c5fa3adf9e42fb1f1f1c7117bf
Matthieu Tanguay-Carel <matthieutc@gmail.com>
2014-01-16 10:18:36 +0100

    initial working presenter

---
+++ present.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+
+require 'haml'
+require 'kramdown'
+
+def present title, content
+  template = File.read('presenter.html.haml')
+  haml_engine = Haml::Engine.new(template)
+  haml_engine.render(Object.new, {slides: content.split(/(?=<h1)/), title: title})
+end
+
+def content_of filename
+  if filename.end_with? ".md"
+    Kramdown::Document.new(File.read(filename)).to_html
+
+  elsif filename.end_with? ".html"
+    File.read(filename)
+
+  else
+    $stderr.puts "Format not supported: #{File.extname(filename)}"
+    exit 1
+  end
+end
+
+def process filename
+  puts present(filename, content_of(filename))
+end
+
+process ARGV[0] if __FILE__ == $0
